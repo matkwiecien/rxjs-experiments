79aef027971688e1d7b1fb2e40784d7fd618c67f
Mateusz Kwiecien <m.kwiecien916@gmail.com>
2023-01-23 15:55:45 +0100

    Init project with counter

---
+++ src/Counter.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { interval } from "rxjs"
+
+const source$ = interval(1000);
+
+ 
+export const Counter = () => {
+    const [ count, setCount ] = useState(0);
+
+    useEffect(() => {
+        const subscription = source$.subscribe((x) => {
+            setCount(x)
+        })
+
+        return () => { subscription.unsubscribe() }
+    }, [source$ ])
+    return <div>{count}</div>
+}
